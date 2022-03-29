d8be22d9af46efc6246b2f8d97703eb72f067162
ChiragSharma1 <chiragsharma1441@gmail.com>
2022-03-29 18:13:18 +0530

    Maps added

--- bank/urls.py
+++ bank/urls.py
@@ -4,5 +4,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('results/', views.results, name='results'),
     path('user/<int:userid>', views.get_user, name='user'),
+    path('bank/<int:bankid>', views.get_bank, name="bank"),
     path('donarform/', views.donarform, name='donar_form')
 ]
--- bank/views.py
+++ bank/views.py
@@ -4,6 +4,7 @@ from .models import User
 import requests
 import json
 from .forms import DonarForm
+import folium
 
 donarblood = {
     'A+': ['A-', 'O+', 'O-'],
@@ -22,7 +23,7 @@ def home(request):
 
 
 def results(request):
-    location = request.GET['location']
+    location = request.GET['location'].title()
     blood = request.GET['blood']
     user_at_location = User.objects.filter(city__iexact=location)
     user_same_blood = user_at_location.filter(blood_group__iexact=blood)
@@ -35,21 +36,34 @@ def results(request):
     print(user_can_donate)
     bldcnt = user_same_blood.count()
     response = requests.get(
-        'https://api.data.gov.in/resource/fced6df9-a360-4e08-8ca0-f283fc74ce15?api-key=579b464db66ec23bdd00000167e6cabfab3040ea41287770d6615098&limit=2800&format=json').json()['records']
+        'https://api.data.gov.in/resource/fced6df9-a360-4e08-8ca0-f283fc74ce15?api-key=579b464db66ec23bdd00000167e6cabfab3040ea41287770d6615098&limit=2800&format=json&filters[__city]='+location).json()['records']
     # print(response)
     bloodbank = []
+    # adding map
+    if len(response) != 0:
+        latitude = float(response[0]['__latitude'])
+        longitude = float(response[0]['__longitude'])
+        m = folium.Map(location=[latitude, longitude], zoom_start=13)
+    else:
+        m = folium.Map()
     for rs in response:
-        if rs['__city'].lower() == location.lower():
-            dic = {
-                'name': rs['_blood_bank_name'],
-                'state': rs['__state'],
-                'city': rs['__city'],
-                'address': rs['__address'],
-                'mobile': rs['_mobile'],
-            }
-            bloodbank.insert(0, dic)
+        dic = {
+            'id': rs['sr_no'],
+            'name': rs['_blood_bank_name'],
+            'state': rs['__state'],
+            'city': rs['__city'],
+            'address': rs['__address'],
+            'mobile': rs['_mobile'],
+        }
+        latitude = float(rs['__latitude'])
+        longitude = float(rs['__longitude'])
+        folium.Marker([latitude, longitude],
+                      tooltip="Click for more",
+                      popup=rs['_blood_bank_name']).add_to(m)
+        bloodbank.insert(0, dic)
 
     # print(bloodbank)
+    m = m._repr_html_()
     bloodbankcnt = len(bloodbank)
     return render(request, 'bank/results.html', {
         'location': location,
@@ -60,6 +74,7 @@ def results(request):
         'cnt': bldcnt,
         'bloodbank': bloodbank,
         'bloodbankcnt': bloodbankcnt,
+        'm': m,
     })
 
 
@@ -82,3 +97,31 @@ def donarform(request):
 
     content = {'form': form}
     return render(request, 'bank/userform.html', content)
+
+
+def get_bank(request, bankid):
+    bankid = str(bankid)
+    bank = requests.get(
+        'https://api.data.gov.in/resource/fced6df9-a360-4e08-8ca0-f283fc74ce15?api-key=579b464db66ec23bdd00000167e6cabfab3040ea41287770d6615098&limit=2800&format=json&filters[sr_no]='+bankid).json()['records'][0]
+    longitude = float(bank['__longitude'])
+    latitude = float(bank['__latitude'])
+    m = folium.Map(location=[latitude, longitude], zoom_start=13)
+    folium.Marker([latitude, longitude],
+                  tooltip="Click for more",
+                  popup=bank['_blood_bank_name']).add_to(m)
+    bank_details = {
+        'name': bank['_blood_bank_name'],
+        'state': bank['__state'],
+        'city': bank['__city'],
+        'pincode': bank['_pincode'],
+        'address': bank['__address'],
+        'email': bank['_email'],
+        'mobile_number': bank['_mobile'],
+        'contact_number': bank['__contact_no'],
+        'timing': bank['__service_time'],
+    }
+    m = m._repr_html_()
+    return render(request, 'bank/bank.html', {
+        'm': m,
+        'bank': bank_details,
+    })
